4b176153a7c73130ebcc29fa494b6710dfe54dda
francescahhh <francesca.c.hamm@gmail.com>
2024-06-12 22:31:58 -0700

    fix logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :notes
   get "/about", to: "about#index"
 
+  devise_scope :user do                                                           
+       get '/users/sign_out' => 'devise/sessions#destroy'  
+       post '/users/sign_out' => 'devise/sessions#destroy'   
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
