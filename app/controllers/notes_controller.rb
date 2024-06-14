e842e6fa3566a5716275245845d46b04f6e24e2a
francescahhh <francesca.c.hamm@gmail.com>
2024-06-14 01:52:03 -0700

    add ability for users to delete their own notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -23,8 +23,18 @@ class NotesController < ApplicationController
     end
   end
 
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy!
+    redirect_to notes_path
+  end
+
   private
     def note_params
       params.require(:note).permit(:title, :body)
+    end
+
+    def user_notes
+      @user_notes = Note.where(user_id: current_user.id)
     end
 end
