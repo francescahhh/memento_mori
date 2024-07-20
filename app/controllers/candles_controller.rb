a4c63cfe9a9390ec98f061ee34ac604c3405d7cd
francescahhh <francesca.c.hamm@gmail.com>
2024-07-19 23:45:21 -0700

    keep pattern consistent in controllers, add notices

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -1,64 +1,45 @@
 class CandlesController < ApplicationController
   before_action :set_candle, only: %i[ show edit update destroy ]
 
-  # GET /candles or /candles.json
   def index
-    @candles = Candle.all
+    @candles = Candle.where(user_id: current_user.id)
   end
 
-  # GET /candles/1 or /candles/1.json
   def show
   end
 
-  # GET /candles/new
   def new
     @candle = Candle.new
   end
 
-  # GET /candles/1/edit
   def edit
   end
 
-  # POST /candles or /candles.json
   def create
     @candle = Candle.new(candle_params)
+    @candle.user = current_user  
 
-    respond_to do |format|
       if @candle.save
-        format.html { redirect_to candle_url(@candle), notice: "Candle was successfully created." }
-        format.json { render :show, status: :created, location: @candle }
+        redirect_to candles_path, notice: "Candle created!"
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @candle.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
     end
-  end
 
-  # PATCH/PUT /candles/1 or /candles/1.json
   def update
-    respond_to do |format|
       if @candle.update(candle_params)
-        format.html { redirect_to candle_url(@candle), notice: "Candle was successfully updated." }
-        format.json { render :show, status: :ok, location: @candle }
+        redirect_to candles_path, notice: "Candle updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @candle.errors, status: :unprocessable_entity }
+         render :edit, status: :unprocessable_entity
       end
     end
-  end
 
-  # DELETE /candles/1 or /candles/1.json
   def destroy
     @candle.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to candles_url, notice: "Candle was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to candles_path, notice: "Candle deleted."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_candle
       @candle = Candle.find(params[:id])
     end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,35 +1,49 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: %i[ show edit update destroy ]
 
   def index
     @notes = Note.all
   end
 
   def show
-    @note = Note.find(params[:id])
   end
 
   def new
     @note = Note.new
   end
 
+  def edit
+  end
+
   def create
     @note = Note.new(note_params)
     @note.user = current_user     
 
-    if @note.save!
-      redirect_to notes_path
+    if @note.save
+      redirect_to notes_path, notice: "Postcard created!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def update
+      if @note.update(note_params)
+        redirect_to notes_path, notice: "Postcard updated."
+      else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
   def destroy
-    @note = Note.find(params[:id])
     @note.destroy!
-    redirect_to notes_path
+    redirect_to notes_path, notice: "Postcard deleted."
   end
 
   private
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
     def note_params
       params.require(:note).permit(:title, :body)
     end
