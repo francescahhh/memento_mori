3a3416f6756afa0c62ed87dd3b97f8a18ee6235e
francescahhh <francesca.c.hamm@gmail.com>
2024-07-21 21:21:10 -0700

    make candle a view component

---
+++ app/components/candle_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class CandleComponent < ViewComponent::Base
+    def initialize(candle:)
+      @candle = candle
+    end
+  end
--- app/views/candles/_candle.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! candle, :id, :name, :lit, :created_at, :updated_at
-json.url candle_url(candle, format: :json)
