6901f0099409957e11656e835708da34b7e664a2
francescahhh <francesca.c.hamm@gmail.com>
2024-07-25 01:14:22 -0700

    clean up redundant line in rails helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,9 +12,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :system 
-  config.before(:each, type: :system) do
-    driven_by :selenium_chrome
-  end
  config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
